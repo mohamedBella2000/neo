c7b21d131fdaf01dbc69bebcc856f71a1e5287b1
mohamed bella <90554606+mohamedBella2000@users.noreply.github.com>
2022-07-05 13:13:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,20 @@ let {
 } = require('child_process'), path = require('path'), CFonts = require('cfonts')
 
 function start() {
-    let t = [path.join(__dirname, 'client.js'), ...process.argv.slice(2)],
-	e = spawn(process.argv[0], t, {
-		stdio: ['inherit', 'inherit', 'inherit', 'ipc']
-	}).on('message', t => {
-		'reset' == t && (e.kill(), start(), delete e)
+	let args = [path.join(__dirname, 'client.js'), ...process.argv.slice(2)]
+	// console.log([process.argv[0], ...args].join('\n'))
+	let p = spawn(process.argv[0], args, { stdio: ['inherit', 'inherit', 'inherit', 'ipc'] })
+	.on('message', data => {
+		if (data == 'reset') {
+			console.log('Restarting...')
+			p.kill()
+			start()
+			delete p
+		}
+	})
+	.on('exit', code => {
+		console.error('Exited with code:', code)
+		if (code == 1) start()
 	})
 }
 
@@ -16,7 +25,7 @@ CFonts.say('NEOXR BOT', {
     font: 'tiny',
     align: 'center',
     colors: ['system']
-}), CFonts.say('Github : https://github.com/neoxr/neoxr-bot', {
+}), CFonts.say('Github : https://github.com/neoxr/neoxr-bot-md', {
     colors: ['system'],
     font: 'console',
     align: 'center'
